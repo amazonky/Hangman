bad1d4fae265707610dd04f8f0d9c565e143f068
unknown <isidoraperovic@icloud.com>
2024-03-18 14:39:45 +0100

    Adding tests

--- test/hangman/core_test.clj
+++ test/hangman/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [hangman.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Test fetch-random-word function
+(deftest test-fetch-random-word
+  (testing "Fetching a random word"
+    (let [word (fetch-random-word)]
+      (is (string? word)))))
+
+
+;; Test character-exists-in-word? function
+(deftest test-character-exists-in-word?
+  (testing "Checking if character exists in a word"
+    (is (character-exists-in-word? "hangman" "h"))
+    (is (not (character-exists-in-word? "hangman" "z")))))
+
+
